a646bcc28d86402eda65eafaade7cd5fce869b02
Liam Niehus-Staab <34754390+niehusst@users.noreply.github.com>
2020-07-19 12:04:41 -0500

    PartyConnectFragment added (#8)
    * adds more gradle deps and enables databinding
    
    * adds timber and nav graph
    
    * starts nav graph and adds rudimentary connect frag
    
    * comments spotify code in mainActivity to use only as future example
    
    * some gradle upgrades/fixes
    
    * fancied party connect layout file
    
    * comment update
    
    * adds ui tests
    
    * updates button style a bit and removes trash file

---
+++ app/src/main/java/com/niehusst/partyq/partyConnect/PartyConnectFragment.kt
@@ -0,0 +1,42 @@
+package com.niehusst.partyq.partyConnect
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.niehusst.partyq.R
+import com.niehusst.partyq.databinding.PartyConnectFragmentBinding
+
+/**
+ * Fragment to start the flow of of connecting a user to an existing party,
+ * or starting their own.
+ */
+class PartyConnectFragment : Fragment() {
+
+    private lateinit var binding: PartyConnectFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = PartyConnectFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // setup click listeners
+        binding.partyJoinButton.setOnClickListener {
+            // TODO: add navigation
+            Toast.makeText(requireContext(), "Not yet implemented", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.partyStartButton.setOnClickListener {
+            // TODO: add navigation
+            Toast.makeText(requireContext(), "Not yet implemented", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
