1cb5c5ef55f4378f2539afb9543bb860b4c1644a
niehusst <liamniehusstaab@gmail.com>
2020-08-01 10:44:09 -0600

    adds nav to about frog for info btn

--- app/src/main/java/com/niehusst/partyq/ui/spotifyLogin/SpotifyLoginFragment.kt
+++ app/src/main/java/com/niehusst/partyq/ui/spotifyLogin/SpotifyLoginFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.niehusst.partyq.R
 import com.niehusst.partyq.databinding.SpotifyLoginFragmentBinding
 import com.niehusst.partyq.services.SpotifyAuthenticationService
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
 class SpotifyLoginFragment : Fragment() {
@@ -35,8 +39,7 @@ class SpotifyLoginFragment : Fragment() {
         }
 
         binding.infoButton.setOnClickListener {
-            // TODO: navigate to about page
-            Toast.makeText(requireContext(), "Not yet implemented", Toast.LENGTH_SHORT).show()
+            view.findNavController().navigate(R.id.aboutFragment)
         }
     }
 
