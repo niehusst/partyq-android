e9471748d8d7de1e42f80a2bf2791e2ec6cc9652
niehusst <liamniehusstaab@gmail.com>
2020-08-01 12:08:10 -0600

    updates key service to hopefully work w/ circle and adds docs

--- app/src/main/java/com/niehusst/partyq/services/KeyFetchService.kt
+++ app/src/main/java/com/niehusst/partyq/services/KeyFetchService.kt
@@ -11,8 +11,22 @@ class KeyFetchService {
 
         private external fun getSpotifyClientKey(): String
 
+        /**
+         * Fetch the Spotify client ID key from a location slightly more secure than
+         * hardcoded right here. It is not included in the source on Github (srry).
+         * If you would like to test code pulled from here on your own, you'll have
+         * to make your own Spotify developer account and get your own client ID.
+         *
+         * @return SPOTIFY_CLIENT_ID - secret key string for authenticating/connecting to
+         *                          the Spotify app via AppRemote SDK
+         */
         fun getSpotifyKey(): String {
-            // TODO: return env var if in circleci?
+            // if running on circleci, load key from env var
+            val envVar = System.getenv("SPOTIFY_CLIENT_ID")
+            if (!envVar.isNullOrEmpty()) {
+                return envVar
+            }
+            // otherwise fetch from "secure location"
             return String(Base64.decode(getSpotifyClientKey(), Base64.DEFAULT))
         }
     }
--- app/src/main/java/com/niehusst/partyq/services/SpotifyAuthenticationService.kt
+++ app/src/main/java/com/niehusst/partyq/services/SpotifyAuthenticationService.kt
@@ -6,23 +6,40 @@ import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import timber.log.Timber
 
-class SpotifyAuthenticationService {
+object SpotifyAuthenticationService {
 
+    // set from the Spotify developer dashboard
     private val clientId = KeyFetchService.getSpotifyKey()
     private val redirectUri = "com.niehusst.partyq://callback"
-    var spotifyAppRemote: SpotifyAppRemote? = null
 
-    private fun authenticateWithSpotfiy(
+    lateinit var spotifyAppRemote: SpotifyAppRemote
+        private set
+
+    /**
+     * Send a request to the Spotify app (must be downloaded on same device as partyq is
+     * being run on) to connect. You must also have authenticated IN the Spotify app
+     * at sometime before (w/ the last 30 days?) this method is called for it to succeed.
+     * If it is the first time for the calling device to authenticate with Spotify, Spotify
+     * will throw up a confirmation screen that you want partyq to have access to your account
+     * data.
+     * This function is run for the side-effect of initializing {spotifyAppRemote}, which
+     * handles all communication with the Spotify app.
+     *
+     * @param context - a valid Android context
+     * @param onConnectCallback - lambda to be called on connection success (optional)
+     * @param onFailCallback - lambda to be called on connection failure (optional)
+     */
+    fun authenticateWithSpotfiy(
         context: Context?,
-        onConnectCallback: (() -> Unit)?,
-        onFailCallback: (() -> Unit)?
+        onConnectCallback: (() -> Unit)? = null,
+        onFailCallback: (() -> Unit)? = null
     ) {
         val connectionParams = ConnectionParams.Builder(clientId)
             .setRedirectUri(redirectUri)
             .showAuthView(true)
             .build()
 
-        SpotifyAppRemote.connect(context, connectionParams, object : Connector.ConnectionListener {
+        SpotifyAppRemote.connect(context!!, connectionParams, object : Connector.ConnectionListener {
             override fun onConnected(appRemote: SpotifyAppRemote) {
                 spotifyAppRemote = appRemote
                 Timber.d("Connected to Spotify!")
