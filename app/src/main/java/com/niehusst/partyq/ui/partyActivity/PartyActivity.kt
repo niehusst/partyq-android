b4d1d38d467b2edcbb39522de1db9a97788f535d
niehusst <liamniehusstaab@gmail.com>
2020-08-23 14:57:15 -0600

    circle fix

--- app/src/main/java/com/niehusst/partyq/ui/partyActivity/PartyActivity.kt
+++ app/src/main/java/com/niehusst/partyq/ui/partyActivity/PartyActivity.kt
@@ -1,17 +1,10 @@
 package com.niehusst.partyq.ui.partyActivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.niehusst.partyq.R
 import com.niehusst.partyq.databinding.ActivityPartyBinding
-import kotlinx.android.synthetic.main.activity_party.*
 import timber.log.Timber
 
 class PartyActivity : AppCompatActivity() {
