6c4c0082cd13dc96926d339e779dc4e242ade6e0
niehusst <liamniehusstaab@gmail.com>
2020-09-08 18:41:46 -0600

    service stubs

---
+++ app/src/main/java/com/niehusst/partyq/services/CommunicationService.kt
@@ -0,0 +1,7 @@
+package com.niehusst.partyq.services
+
+object CommunicationService {
+    // TODO: connect to party net
+    // TODO: send/get search
+    // TODO: update queue
+}
---
+++ app/src/main/java/com/niehusst/partyq/services/SpotifyPlayer.kt
@@ -0,0 +1,5 @@
+package com.niehusst.partyq.services
+
+object SpotifyPlayer {
+    // TODO: only actually PLAY the music if user is the host
+}
