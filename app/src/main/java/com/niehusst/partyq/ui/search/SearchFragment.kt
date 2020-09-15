fec2dc7674d91ea644973788c9aa82036c7818c8
niehusst <liamniehusstaab@gmail.com>
2020-09-15 09:49:46 -0600

    fixes keyboard view pushing and keyboard persitence issue

--- app/src/main/java/com/niehusst/partyq/ui/search/SearchFragment.kt
+++ app/src/main/java/com/niehusst/partyq/ui/search/SearchFragment.kt
@@ -1,17 +1,19 @@
 package com.niehusst.partyq.ui.search
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.SearchView
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.niehusst.partyq.databinding.SearchFragmentBinding
+
 
 class SearchFragment : Fragment() {
 
@@ -33,7 +35,6 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.searchResults.adapter = adapter
-        binding.searchBar.clearFocus() // clear initial focus on EditText
         setObserver()
         setSearchListener()
     }
@@ -49,11 +50,17 @@ class SearchFragment : Fragment() {
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 // send query to Spotify
                 // TODO: start loading spinner
-                // TODO: close keyboard
+                hideKeyboardFrom(v)
                 viewModel.submitQuery(v.text.toString())
-                Toast.makeText(requireContext(), "Entered serach", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Entered serach ${v.text.toString()}", Toast.LENGTH_SHORT).show()
             }
             return@setOnEditorActionListener true
         }
+    }
+
+    fun hideKeyboardFrom(view: View) {
+        val imm: InputMethodManager =
+            requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
