1a047724d39a76af829f6bbe0bfd9cf098bee260
niehusst <liamniehusstaab@gmail.com>
2020-11-05 18:36:52 -0700

    legal frag looks ok now

--- app/src/main/java/com/niehusst/partyq/ui/legal/LegalFragment.kt
+++ app/src/main/java/com/niehusst/partyq/ui/legal/LegalFragment.kt
@@ -1,6 +1,7 @@
 package com.niehusst.partyq.ui.legal
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,22 @@ import com.niehusst.partyq.databinding.LegalFragmentBinding
 
 class LegalFragment : BottomSheetDialogFragment() {
 
+    private lateinit var binding: LegalFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return LegalFragmentBinding.inflate(layoutInflater).root
+        binding = LegalFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // link github URL
+        binding.partyqLicText.movementMethod = LinkMovementMethod.getInstance()
     }
 
     companion object {
